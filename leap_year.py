0e4707d49a642e80f328d9cecb7f001eb954994e
tomasgagliardi <tgagliardi@mail.austral.edu.ar>
2025-04-14 15:58:45 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    año = int(imput("Ingrese un año: "))
+    if(año%4==0) and(año%100 ! =0):
+        print(f"El año {año} es biciesto")
+    elif año%400==0: 
+        print(f"El año {año} es biciesto")
+    else:
+        print(f"El año {año} no es biciesto")
